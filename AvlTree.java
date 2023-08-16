0593ce2ea70f936aa2284b34bc962776c8761499
aaronworsnop <aaronworsnop@gmail.com>
2023-08-16 23:44:22 +1200

    Finished new implementation take 1

--- AvlTree.java
+++ AvlTree.java
@@ -20,6 +20,14 @@ public class AvlTree {
     root = remove(data, root);
   }
 
+  public int max() {
+    return max(root);
+  }
+
+  public int min() {
+    return min(root);
+  }
+
   private AvlNode add(int data, AvlNode node) {
     if (node == null) {
       return new AvlNode(data);
@@ -55,5 +63,63 @@ public class AvlTree {
     }
     updateHeight(node);
     return rotate(node);
+  }
+
+  private int max(AvlNode node) {
+    if (node.getRight() == null) {
+      return node.getData();
+    }
+    return max(node.getRight());
+  }
+
+  private int min(AvlNode node) {
+    if (node.getLeft() == null) {
+      return node.getData();
+    }
+    return min(node.getLeft());
+  }
+
+  private AvlNode rotate(AvlNode node) {
+    if (balance(node) > 1) {
+      // Left side longer
+      if (balance(node.getLeft()) < 0) {
+        // Left right case
+        node.setLeft(rotateLeft(node.getLeft()));
+      }
+      return rotateRight(node);
+    }
+    if (balance(node) < -1) {
+      // Right side longer
+      if (balance(node.getRight()) > 0) {
+        // Right left case
+        node.setRight(rotateRight(node.getRight()));
+      }
+      return rotateLeft(node);
+    }
+    return node;
+  }
+
+  private AvlNode rotateLeft(AvlNode node) {
+    node.getRight().setLeft(node);
+    node.setRight(node.getRight().getLeft());
+    updateHeight(node);
+    updateHeight(node.getRight());
+    return node.getRight();
+  }
+
+  private AvlNode rotateRight(AvlNode node) {
+    node.getLeft().setRight(node);
+    node.setLeft(node.getLeft().getRight());
+    updateHeight(node);
+    updateHeight(node.getLeft());
+    return node.getLeft();
+  }
+
+  private void updateHeight(AvlNode node) {
+    node.setHeight(Math.max(height(node.getLeft()), height(node.getRight())) + 1);
+  }
+
+  private int height(AvlNode node) {
+    return node != null ? node.getHeight() : 0;
   }
 }
