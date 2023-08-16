29353d00893b368a02c437dc49b399d825a0c878
aaronworsnop <aaronworsnop@gmail.com>
2023-08-17 00:25:11 +1200

    test: Further testing with comments to illustrate what's giong on

--- AvlMain.java
+++ AvlMain.java
@@ -5,15 +5,65 @@ public class AvlMain {
 
     System.out.println(tree.getRoot().getHeight());
 
-    tree.add(15).add(5).add(17).add(16);
+    tree.add(15).add(5).add(17).add(16).add(13).add(18).add(24).add(26);
 
     System.out.println(tree.getRoot().getHeight());
-    System.out.println(tree.getRoot().getData());
+    System.out.println("root: " + tree.getRoot().getData());
     System.out.println(tree.getRoot().getLeft().getData());
     System.out.println(tree.getRoot().getLeft().getLeft().getData());
-    // System.out.println(tree.getRoot().getLeft().getRight().getData()); -> null
+    System.out.println(tree.getRoot().getLeft().getRight().getData());
     System.out.println(tree.getRoot().getRight().getData());
     System.out.println(tree.getRoot().getRight().getLeft().getData());
     System.out.println(tree.getRoot().getRight().getRight().getData());
+    System.out.println(tree.getRoot().getRight().getRight().getRight().getData());
+    System.out.println(tree.getRoot().getRight().getLeft().getLeft().getData());
+
+    // 10
+
+    // 10
+    //  \
+    //   15
+
+    //   10
+    //  /  \
+    // 5   15
+
+    //     10
+    //    /  \
+    //   5   15
+    //         \
+    //         17
+
+    //     10
+    //    /  \
+    //   5   15
+    //         \
+    //         17  // Time to rotate
+    //         /
+    //        16
+
+    //     15
+    //    /  \
+    //   10   17  // After rotation
+    //  /    /
+    // 5    16
+
+    //     15
+    //    /  \
+    //   10   17
+    //  /\    /\
+    // 5 13  16 18  // Time to rotate
+    //           \
+    //           24
+    //            \
+    //            26
+
+    //     15
+    //    /  \
+    //   10   18
+    //  /\    /\   // After rotation
+    // 5 13  17 24
+    //       /   \
+    //      16   26
   }
 }
