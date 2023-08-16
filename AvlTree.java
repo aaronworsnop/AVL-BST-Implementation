3c80c39325c695270b43a6838772b10b0591b65f
aaronworsnop <aaronworsnop@gmail.com>
2023-08-16 23:33:48 +1200

    Remove method

--- AvlTree.java
+++ AvlTree.java
@@ -16,6 +16,10 @@ public class AvlTree {
     return this;
   }
 
+  public void remove(int data) {
+    root = remove(data, root);
+  }
+
   private AvlNode add(int data, AvlNode node) {
     if (node == null) {
       return new AvlNode(data);
@@ -26,6 +30,28 @@ public class AvlTree {
       node.setRight(add(data, node.getRight()));
     } else {
       return node;
+    }
+    updateHeight(node);
+    return rotate(node);
+  }
+
+  private AvlNode remove(int data, AvlNode node) {
+    if (node == null) {
+      return null;
+    }
+    if (data < node.getData()) {
+      node.setLeft(remove(data, node.getLeft()));
+    } else if (data > node.getData()) {
+      node.setRight(remove(data, node.getRight()));
+    } else {
+      if (node.getLeft() == null) {
+        return node.getRight();
+      } else if (node.getRight() == null) {
+        return node.getLeft();
+      }
+        node.setData(max(node.getLeft()));
+        node.setLeft(remove(node.getData(), node.getLeft()));
+      }
     }
     updateHeight(node);
     return rotate(node);
